bd3a223a923de1cc9fcc290969b0b14fc133ea8d
Surya Prakash <suryasrinivas765@gmail.com>
2025-06-30 01:24:00 +0530

    creating SG

--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -10,3 +10,25 @@ module "frontend" {
   vpc_id = local.vpc_id
 
 }
+
+module "bastion" {
+  #source = "../../terraform-aws-securitygroup"
+  source = "git::https://github.com/KosaraSurya/terraform-aws-securitygroup.git?ref=main"
+  project = var.project
+  environment = var.environment
+
+  sg_name = var.bastion_sg_name
+  sg_description = var.bastion_sg_description
+  #vpc_id = data.aws_ssm_parameter.vpc_id.value
+  vpc_id = local.vpc_id
+
+}
+
+
+output "frontend_sg_id" {
+  value = module.frontend.sg_id
+}
+
+output "bastino_sg_id" {
+  value = module.bastion.sg_id 
+}
