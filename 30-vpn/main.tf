2db341cdfa4b7d5128568fd3e7d95654ac38e4c1
KosaraSurya <suryasrinivas765@gmail.com>
2025-07-09 01:27:49 +0530

    30-vpn

---
+++ 30-vpn/main.tf
@@ -0,0 +1,25 @@
+/* resource "aws_key_pair" "openvpn" {
+  key_name   = "openvpn"
+  public_key = "file(C:\\devops\\openvpn.pub)"
+} */
+
+resource "aws_instance" "vpn" {
+  ami           = local.ami_id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [local.vpn_sg_id]
+  subnet_id = local.public_subnet_id
+  key_name = "dws84" # Make sure this is exists in AWS account
+  #key_name = aws_key_pair.openvpn.key_name # If key no there in aws, Create resource in terraform and importr like this
+  user_data = file("openvpn.sh") # for headless mode
+  
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.project}-${var.environment}-VPN"
+    }
+  )
+}
+
+output "vpnEc2" {
+  value = aws_instance.vpn.id
+}
