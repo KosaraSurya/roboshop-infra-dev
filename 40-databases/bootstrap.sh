5410fb4c68d1347ec9c95859c4178d20646564e0
KosaraSurya <suryasrinivas765@gmail.com>
2025-07-09 06:47:49 +0530

    40-databases

---
+++ 40-databases/bootstrap.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+dnf install ansible -y
+ansible-pull -u https://github.com/KosaraSurya/ansible-roles-roboshop.git -e component=$1 main.yaml
+# component given because in ansible we written same playbbok for all in run time we are passing args as component
