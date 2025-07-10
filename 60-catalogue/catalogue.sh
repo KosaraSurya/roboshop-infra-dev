854ca92515ea50609e3cb8af5fe4df2d193d5dbc
KosaraSurya <suryasrinivas765@gmail.com>
2025-07-10 23:44:46 +0530

    60-catalogue

---
+++ 60-catalogue/catalogue.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+dnf install ansible -y
+ansible-pull -U https://github.com/KosaraSurya/ansible-roles-roboshop-tf.git -e component=$1 -e env= $2 main.yaml
+# component given because in ansible we written same playbbok for all in run time we are passing args as component
